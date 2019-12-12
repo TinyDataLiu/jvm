7a228acd1949c58b3647811dc569615725682fda
卖小姑娘的火柴 <1360737271@qq.com>
2019-12-12 21:40:26 +0800

    简单理解下虚拟机栈，栈帧

---
+++ jvm-01/src/main/java/com/alice/jvm/frames/FramesSimple.java
@@ -0,0 +1,12 @@
+package com.alice.jvm.frames;
+
+/**
+ * @author liuchun
+ * @date 2019/12/12  15:33
+ */
+public class FramesSimple {
+    public int add(int v1, int v2) {
+        int v3 = v1 + v2;
+        return v3;
+    }
+}
