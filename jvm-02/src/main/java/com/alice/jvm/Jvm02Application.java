e9961cf8f89f111a7c5fb4a681579839a5ccfadf
liuchun <1360737271@qq.com>
2019-12-19 09:26:53 +0800

    Jvm Test

--- jvm-02/src/main/java/com/alice/jvm/Jvm02Application.java
+++ jvm-02/src/main/java/com/alice/jvm/Jvm02Application.java
@@ -1,5 +1,7 @@
 package com.alice.jvm;
 
+import com.alice.jvm.bean.Emp;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
@@ -7,23 +9,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
+@Slf4j
 @SpringBootApplication
 public class Jvm02Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Jvm02Application.class, args);
+        log.info("server start");
     }
 
     @Controller
     class Heap {
 
-        private List<Object> list = new ArrayList<>();
+        private List<Emp> list = new ArrayList<>();
 
         @RequestMapping("heap")
         public void heap() {
+            int i = 0;
             while (true) {
-                list.add(new Object());
+                list.add(Emp.builder().id(++i).name(UUID.randomUUID().toString()).build());
             }
         }
     }
