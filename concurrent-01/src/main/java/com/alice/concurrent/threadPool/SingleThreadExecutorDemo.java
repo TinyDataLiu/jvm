27f2bac3cc3554f2b7821892174b10ed96e79f57
liuchun <1360737271@qq.com>
2020-02-22 16:49:07 +0800

    线程池

--- concurrent-01/src/main/java/com/alice/concurrent/threadPool/SingleThreadExecutorDemo.java
+++ concurrent-01/src/main/java/com/alice/concurrent/threadPool/SingleThreadExecutorDemo.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * @author liuchun
@@ -27,6 +28,8 @@ public class SingleThreadExecutorDemo implements Runnable {
 
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
+            ThreadPoolExecutor executor = (ThreadPoolExecutor) executorService;
+            executor.allowCoreThreadTimeOut(Boolean.TRUE);
             executorService.execute(new SingleThreadExecutorDemo(i));
         }
     }
