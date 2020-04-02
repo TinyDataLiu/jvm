74a89ddf8bced0853cebcaf1f1af7cbffe6673b2
卖小姑娘的火柴 <1360737271@qq.com>
2020-04-02 17:23:22 +0800

    jvm 杂记

--- jvm-02/src/test/java/com/alice/jvm/Jvm02ApplicationTests.java
+++ jvm-02/src/test/java/com/alice/jvm/Jvm02ApplicationTests.java
@@ -1,13 +1,13 @@
-package com.alice.jvm;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class Jvm02ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.alice.jvm;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class Jvm02ApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
